a473141386b7a00ad0572c7527c9ed8987191ba7
Li-Na-Yeh <41134312+Li-Na-Yeh@users.noreply.github.com>
2018-08-05 16:19:27 -0700

    update

---
+++ readme.ps1
@@ -0,0 +1,29 @@
+## this is my notebook ##
+
+#for Syntax symbols
+#(  //  Comments - Freely place anything in here
+/*
+* Block comment
+* 
+*/
+// ; - End the sentence or statement
+// "String text"
+// + - * / % - arithmetic
+// () - method call
+// [] - array (lists)
+// () - Start and end points of code (blocks)
+// > >= < <= && || ! - Comparisions
+)
+
+#for Grammar
+(  // Guidelines / Rules
+
+// class Player
+// class PlayerController
+// class Player_Controller
+
+// int lives
+// int livesMax
+
+// void Jump ()
+}   // void Jumpstarted ())
